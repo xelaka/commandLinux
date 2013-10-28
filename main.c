40759ea27fe43beb0a5c238387752ba087a64d3c
xelaka <sebastien.rl94@gmail.com>
2013-10-28 18:33:38 +0100

    je commit

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main(int argc, char** argv){
 	printf("diff !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-	printf("bonjour ca va ?");
+	printf("bonjour ca va ? tu commit puis tu git push origin master !");
 	return 0;
 }
